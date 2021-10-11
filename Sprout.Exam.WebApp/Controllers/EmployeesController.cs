450f39869fd77a6de1d9b5e8df15d180f76a24fa
Archie33087 <archie.gloria@victory.org.ph>
2021-10-11 14:17:56 +0800

    Initial Commit

---
+++ Sprout.Exam.WebApp/Controllers/EmployeesController.cs
@@ -0,0 +1,168 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Sprout.Exam.Business.DataTransferObjects;
+using Sprout.Exam.Common.Enums;
+using Sprout.Exam.WebApp.Data;
+using Sprout.Exam.WebApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Globalization;
+
+namespace Sprout.Exam.WebApp.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : Controller
+    {
+        private readonly EmployeeContext _context;
+
+        public EmployeesController(EmployeeContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Refactor this method to go through proper layers and fetch from the DB.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            //var result = await Task.FromResult(StaticEmployees.ResultList);
+            try
+            {
+                var result = _context.Employees.ToList();
+
+                foreach (Employee emp in result)
+                {
+                    emp.Birthdate = DateTime.Parse(emp.Birthdate.ToString("yyyy-MM-dd"));
+                }
+
+                return Ok(result);
+            } catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error retrieving the data. " + ex.Message + 
+                        "Try again, and if the problem persists " +
+                        "see your system administrator.");
+
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Refactor this method to go through proper layers and fetch from the DB.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            //var result = await Task.FromResult(StaticEmployees.ResultList.FirstOrDefault(m => m.Id == id));
+            var result = await _context.Employees
+                                    .AsNoTracking()
+                                    .FirstOrDefaultAsync(m => m.Id == id);
+
+            //result.Birthdate = result.Birthdate.ToString("yyyy-MM-dd");
+                            
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// Refactor this method to go through proper layers and update changes to the DB.
+        /// </summary>
+        /// <returns></returns>
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(EditEmployeeDto input)
+        {
+            var item = await Task.FromResult(StaticEmployees.ResultList.FirstOrDefault(m => m.Id == input.Id));
+            if (item == null) return NotFound();
+            item.FullName = input.FullName;
+            item.Tin = input.Tin;
+            item.Birthdate = input.Birthdate;
+            item.EmployeeTypeId = input.EmployeeTypeId;
+            return Ok(item);
+        }
+
+        /// <summary>
+        /// Refactor this method to go through proper layers and insert employees to the DB.
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> Post(CreateEmployeeDto input)
+        {
+            try
+            {
+                var employee = new Employee();
+
+                employee.FullName       = input.FullName;
+                employee.Birthdate      = input.Birthdate;
+                employee.Tin            = input.Tin;
+                employee.EmployeeTypeId = input.EmployeeTypeId;
+
+                if (ModelState.IsValid)
+                {
+                    _context.Add(employee);
+                    await _context.SaveChangesAsync();
+                    return Created($"/api/employees/{employee.Id}", employee.Id);
+                }
+
+                //return Created($"/api/employees/{employee.Id}", employee.Id);
+            }
+            catch (DbUpdateException ex)
+            {
+                ModelState.AddModelError("", "Unable to save changes. " +
+                        "Try again, and if the problem persists " +
+                        "see your system administrator.");
+            }
+            return null;
+        }
+
+
+        /// <summary>
+        /// Refactor this method to go through proper layers and perform soft deletion of an employee to the DB.
+        /// </summary>
+        /// <returns></returns>
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await Task.FromResult(StaticEmployees.ResultList.FirstOrDefault(m => m.Id == id));
+            if (result == null) return NotFound();
+            StaticEmployees.ResultList.RemoveAll(m => m.Id == id);
+            return Ok(id);
+        }
+
+
+
+        /// <summary>
+        /// Refactor this method to go through proper layers and use Factory pattern
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="absentDays"></param>
+        /// <param name="workedDays"></param>
+        /// <returns></returns>
+        [HttpPost("{id}/calculate")]
+        public async Task<IActionResult> Calculate(int id,decimal absentDays,decimal workedDays)
+        {
+            var result = await Task.FromResult(StaticEmployees.ResultList.FirstOrDefault(m => m.Id == id));
+
+            if (result == null) return NotFound();
+            var type = (EmployeeType) result.EmployeeTypeId;
+            return type switch
+            {
+                EmployeeType.Regular =>
+                    //create computation for regular.
+                    Ok(25000),
+                EmployeeType.Contractual =>
+                    //create computation for contractual.
+                    Ok(20000),
+                _ => NotFound("Employee Type not found")
+            };
+
+        }
+
+    }
+}
---
+++ Sprout.Exam.WebApp/Data/EmployeeContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Sprout.Exam.WebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Sprout.Exam.WebApp.Data
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().ToTable("Employee");
+        }
+    }
+}
